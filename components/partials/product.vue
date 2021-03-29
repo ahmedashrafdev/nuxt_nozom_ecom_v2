e9cceed628c389a22fbad233e8142ce0e4da3f8f
ahmedashrafdev <ahmad.ashraf.dev@gmail.com>
2021-03-29 14:26:08 +0200

    product wireframe

--- components/partials/product.vue
+++ components/partials/product.vue
@@ -1,6 +1,7 @@
 <template>
      <v-skeleton-loader
         class="mx-auto"
+        @click.prevent="$router.push({name : `shop-id___${$i18n.locale}` , params :{id : 1}})"
         type="card, list-item-two-line"
       ></v-skeleton-loader>
 </template>
--- pages/shop/_id.vue
+++ pages/shop/_id.vue
@@ -1,14 +1,97 @@
 <template>
+<div class="shop">
   <v-row justify="center" align="center">
+   <v-col cols="12 p-0">
+     <layouts-breadcrumbs/>
+   </v-col>
     <v-col cols="12">
-      Product
+      <v-row>
+        <v-col cols="12">
+          <v-skeleton-loader
+            width="100%"
+            type="image"
+          >
+          </v-skeleton-loader>
+        </v-col>
+        <v-col cols="12">
+          <h2 class="product-title larger mb-2">name</h2>
+         
+          <p class="p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quibusdam quos? Minima iure repellendus quia dignissimos perspiciatis similique consequatur optio reiciendis, repudiandae aperiam iste aspernatur, dicta asperiores corrupti distinctio soluta?
+          </p>
+          <div class="price  text-h4">
+            <span class="text-primary">{{ $n(100, 'currency' , 'en') }}</span>
+            <del>{{ $n(120, 'currency' , 'en') }}</del>
+          </div>
+          <div class="qty">
+            <div class="d-flex atc items-center py-2">
+              <v-icon @click.prevent="qty++" small>mdi-plus</v-icon>
+              <v-text-field class="atc-input"  v-model="qty"/>
+              <v-icon @click.prevent="qty--" small>mdi-minus</v-icon>
+            </div>
+          </div>
+          <div class="btns d-flex">
+            <v-btn text  class="border  p-4 rounded-xl">
+              <v-icon>mdi-cart-arrow-down</v-icon>
+              {{$t('add_to_cart')}}
+            </v-btn>
+             <v-btn
+              class="mx-2 p-4  border"
+              fab
+              text
+              light
+              small
+            >
+              <v-icon light>
+                mdi-heart
+              </v-icon>
+            </v-btn>
+          </div>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
+  <modals-mobile-filters/>
+</div>
 </template>
 
 <script>
-
+import { mapGetters } from 'vuex'
 export default {
+  mounted(){
+    console.log(this.$route.name)
+  },
+  computed: {
+    ...mapGetters({
+      products: 'shop/products'
+    })
+
+  },
+  data(){
+    return {
+      page: 1,
+      qty : 1,
+      sorting:'Foo',
+      items: ['Foo', 'Bar', 'Fizz', 'Buzz'],
+    }
+  },
+  methods:{
+    openFiltersModal(){
+				this.$store.commit('ui/mobileFiltersModal', true)
+
+    }
+  }
   
 }
 </script>
+
+<style scoped>
+.applied-filters div:not(:last-child){
+  margin: 0 10px 10px 0;
+}
+.showing{
+  border-top: 1px solid var(--border-color);
+  border-bottom: 1px solid var(--border-color);
+  padding: 10px 0;
+}
+</style>
